a49e56266df2636ba16f5e1b3f76392ded7dce64
iustc <imarklei90@126.com>
2019-05-28 09:37:40 +0800

    [Develop] Commit Scala Map Function

--- Scala/src/main/scala/Scala04_Map.scala
+++ Scala/src/main/scala/Scala04_Map.scala
@@ -8,7 +8,9 @@ import scala.collection.mutable
   *
   */
 object Scala04_Map {
-    def main(args: Array[String]): Unit = {
+
+
+  def main(args: Array[String]): Unit = {
 
       /**
         * Scala中不可变的Map是有序的（默认），可变的Map是无序的
@@ -18,7 +20,7 @@ object Scala04_Map {
       println("Map1:" + map1)
       val value = map1("Mark")
       println(value)
-      println("get().get : " + map1.get("Mark").get)
+      println("get().get : " + map1.get("Mark"))
       println("getOrElse : " + map1.getOrElse("aaa", "aaa"))
 
       // 构造可变映射
@@ -66,5 +68,55 @@ object Scala04_Map {
         println(v)
       }
 
+      // Map映射高阶函数
+      testMapAdvanced()
+
+      testFlatMap()
+
+      testFilter()
+
     }
+
+  /**
+    * Map高阶函数
+    */
+    def testMapAdvanced(): Unit ={
+      val list = List(1,2,3,4,5)
+
+      val list2 = list.map(multiple)
+      println("List 2: " + list2)
+
+    }
+
+    def multiple(n : Int):Int ={
+      2 * n
+    }
+
+    /**
+      * FlatMap : 将集合中的每一个元素的子元素映射到某个函数并返回新的集合
+      */
+    def testFlatMap(): Unit ={
+      val names = List("A", "B", "C")
+      val names2 = names.flatMap(upper)
+      println("Names : " + names2)
+
+    }
+
+    def upper(s : String): String ={
+      s.toUpperCase
+    }
+
+    def testFilter(): Unit ={
+      val names = List("Alice", "Bob", "Mark", "Mic")
+
+      val names2 = names.filter(startA)
+
+      println("Names2 :" + names2)
+    }
+
+    def startA(str : String): Boolean ={
+      str.startsWith("M")
+    }
+
+
 }
